d45e62fc26f15fa311ebbd1a39c1ce824a79fcd4
dawood <dawood.altai111@gmail.com>
2023-04-13 18:37:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.querySelector("#main")
+main.remove();
+
+const newHeader = document.createElement("h1");
+ newHeader.id="victory";
+ newHeader.innerHTML="YOUR-NAME is the champion"
+// newHeader.setAttribute('id', 'victory');
+document.body.append(newHeader);
+
+
